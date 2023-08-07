08c1d39cb256e392688765f5960c99b3db401ad1
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-08-07 16:08:15 +0900

    chore ContextAPI 및 useState Hooks

--- todoapp/src/components/TodoCreate.js
+++ todoapp/src/components/TodoCreate.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdAdd } from "react-icons/md";
 
+import { useTodoDispatch, useTodoNextId } from "../Context/TodoContext";
 import {
   CircleButton,
   InsertFormPositioner,
