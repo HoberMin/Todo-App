6e706b606e84f080d8d675a2d6a3d8646ff205e4
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-08-07 16:12:58 +0900

    feat Head에 현재날짜 표시 기능

--- todoapp/src/components/TodoHead.js
+++ todoapp/src/components/TodoHead.js
@@ -6,8 +6,19 @@ function TodoHead() {
   const todos = useTodoState();
   const undoneTasks = todos.filter((todo) => !todo.done);
 
+  const today = new Date();
+  const dateString = today.toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  const dayName = today.toLocaleDateString("ko-KR", { weekday: "long" });
+
   return (
     <TodoHeadBlock>
+      <h1>{dayName}</h1>
+      <div className="day">{dateString}</div>
       <div className="tasks-left">할일 남은 개수 : {undoneTasks.length} </div>
     </TodoHeadBlock>
   );
