ed1b5036f5302ba4df34449281b41cc878bc93d0
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-08-07 18:00:15 +0900

    feat todoCreate POST 요청 핸들러

--- todoapp/src/mocks/handlers.js
+++ todoapp/src/mocks/handlers.js
@@ -7,5 +7,11 @@ const handlers = [
     return res(ctx.status(200), ctx.json(req.body));
   }),
 
+  rest.post("/api/todo", (req, res, ctx) => {
+    // 요청 정보를 콘솔에 출력합니다.
+    console.log("Todolist 생성", req.body);
+    return res(ctx.status(200), ctx.json(req.body));
+  }),
+
 
 export default handlers;
