d71f5546dd088afe434daae8c5a58703a86fa29e
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-08-07 18:00:36 +0900

    feat Todo DELETE 요청 핸들러

--- todoapp/src/mocks/handlers.js
+++ todoapp/src/mocks/handlers.js
@@ -13,5 +13,11 @@ const handlers = [
     return res(ctx.status(200), ctx.json(req.body));
   }),
 
+  rest.delete("/api/todo", (req, res, ctx) => {
+    // 요청 정보를 콘솔에 출력합니다.
+    console.log("TodoItem 삭제:", req.body);
+    return res(ctx.status(200), ctx.json({ message: "삭제성공" }));
+  }),
+
 
 export default handlers;
