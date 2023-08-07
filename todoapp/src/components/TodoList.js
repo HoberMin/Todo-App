f453e02984a54456e72094e8aef3d1fb40fe4cad
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-08-07 16:22:50 +0900

    feat Todo리스트 가시화 컴포넌트 구현

--- todoapp/src/components/TodoList.js
+++ todoapp/src/components/TodoList.js
@@ -1,14 +1,25 @@
 import React from "react";
 import TodoItem from "./TodoItem";
+import { useTodoState } from "../Context/TodoContext";
 import { TodoListBlock } from "../styled/Styled";
 
 function TodoList() {
+  const todos = useTodoState();
+
   return (
     <TodoListBlock>
       <TodoItem text="프로젝트 생성하기" done={true} />
       <TodoItem text="컴포넌트 스타일링 하기" done={true} />
       <TodoItem text="Context 만들기" done={false} />
       <TodoItem text="기능 구현하기" done={false} />
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          id={todo.id}
+          text={todo.text}
+          done={todo.done}
+        />
+      ))}
     </TodoListBlock>
   );
 }
