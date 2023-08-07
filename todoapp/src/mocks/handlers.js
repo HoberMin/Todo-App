5f86c97f61d0d7c088f9993dd6aaf75e5db5fefe
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-08-07 17:59:47 +0900

    feat login POST 요청 핸들러

--- todoapp/src/mocks/handlers.js
+++ todoapp/src/mocks/handlers.js
@@ -1,4 +1,11 @@
 import { rest } from "msw";
 
+const handlers = [
+  rest.post("/api/login", (req, res, ctx) => {
+    // 요청 정보를 콘솔에 출력합니다.
+    console.log("로그인 요청:", req.body);
+    return res(ctx.status(200), ctx.json(req.body));
+  }),
+
 
 export default handlers;
