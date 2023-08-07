13671801fee75ef212bf73d9e5d69ff63e5714b9
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-08-07 16:14:24 +0900

    chore useState Hooks icon 모듈 Context API 호출

--- todoapp/src/components/TodoItem.js
+++ todoapp/src/components/TodoItem.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { MdDone, MdDelete } from "react-icons/md";
+import React, { useState } from "react";
+import { MdDone, MdDelete, MdRefresh } from "react-icons/md";
+import { useTodoDispatch, useTodoNextId } from "../Context/TodoContext";
 // 아이콘 모듈
 import { Remove, TodoItemBlock, CheckCircle, Text } from "../styled/Styled";
 
 function TodoItem({ id, done, text }) {
+  const dispatch = useTodoDispatch();
+  const nextId = useTodoNextId();
   return (
     <TodoItemBlock>
       <CheckCircle done={done}>{done && <MdDone />}</CheckCircle>
       <Text done={done}>{text}</Text>
       <Remove>
+        {done && <MdDone />}
+        <MdRefresh />
         <MdDelete />
       </Remove>
     </TodoItemBlock>
